340c4a6402ae4e620ed61b4c47bca2c7d7a68a75
roberto.ramos <vANILL@ICE101>
2022-04-16 16:22:44 -0300

    configurações e base de dados

---
+++ src/test/java/resources/inclusao_dados_base.sql
@@ -0,0 +1,15 @@
+insert into pessoa(
+	id_pessoa, nome, cpf, dataNascimento, dataCriacao)
+Values(
+	1, 'Pessoa Jose', '24876798087', '1976-02-09 00:00:00.000', CURRENT_TIMESTAMP());
+
+insert into pessoa(
+	id_pessoa, nome, cpf, dataNascimento, dataCriacao)
+Values(
+	1, 'Pessoa Amauri', '61853909068', '1986-11-25 00:00:00.000', CURRENT_TIMESTAMP());
+
+insert into pessoa(
+	id_pessoa, nome, cpf, dataNascimento, dataCriacao)
+Values(
+	1, 'Pessoa Carlos', '92440231053', '1996-03-19 00:00:00.000', CURRENT_TIMESTAMP());
+
